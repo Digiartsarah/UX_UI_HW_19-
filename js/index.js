3cfd933801b026f9643ed9f7bed05734185930f0
Digiartsarah <84052077+Digiartsarah@users.noreply.github.com>
2021-10-05 23:23:19 -0700

    UX_UI_HW_19-
    HW19

---
+++ js/index.js
@@ -0,0 +1,12 @@
+console.log("Your index.js file is loaded correctly!");
+<script>
+    $(document).ready(function(){
+$('See Project 1').click(function(){});
+  alert('Button Clicked');
+$('See Project 2').click(function(){});
+  alert('Button Clicked');
+$('See Project 3').click(function(){});
+  alert('Button Clicked');
+
+    
+</script>
